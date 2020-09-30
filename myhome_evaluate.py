33d19063b3678b9bed2a7223808aff669de22897
Wadim <w.j.smuda@gmail.com>
2020-09-30 14:02:11 +0200

    added different average types

--- myhome_evaluate.py
+++ myhome_evaluate.py
@@ -1,6 +1,11 @@
 import json
+import os
+import math
+
+ga_square=[]
 
 def read_data(filename):
+    global ga_square
     with open(filename) as f:
         data = json.load(f)["Data"]
         
@@ -13,12 +18,25 @@ def read_data(filename):
         }
         for entry in data["Prs"]
     ]
-    print(products)
+    #square= round(sum([dicts['square_price'] for dicts in products])/len([dicts['square_price'] for dicts in products]),2) # not useful for adjusted average  
+    ga_square += [dicts['square_price'] for dicts in products]
 
-    
 
 
 summary = [
-    read_data(f"data-{i}.json")
-    for i in range(1)
+    read_data(f"history/data-{i}.json")
+    for i in range(len([1 for x in list(os.scandir("./history")) if x.is_file()]))
 ]
+
+average_square = sum(ga_square)/len(ga_square)
+ga_square.sort()
+median_square = ga_square[math.floor(len(ga_square)/2)]
+
+print("average square total: " + str(average_square))
+print("median total: " + str(median_square))
+cut = ga_square[math.floor(0.05*len(ga_square)):math.floor(0.95*len(ga_square))]
+adjusted_average_square_price= sum(cut)/len(cut)
+print("adjusted average: " + str(adjusted_average_square_price))
+print(len(ga_square))
+print(len(cut))
+print(cut)
