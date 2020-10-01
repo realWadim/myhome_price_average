88a0240adf468fb2b99dc993c12d1b1ca26ab90b
Wadim <w.j.smuda@gmail.com>
2020-10-01 14:12:50 +0200

    finished the loop to get average size,square meter price and total prices

--- myhome_evaluate.py
+++ myhome_evaluate.py
@@ -16,55 +16,36 @@ def read_data(filename):
         for entry in data["Prs"]
     ]
     return products
-    #square= round(sum([dicts['square_price'] for dicts in products])/len([dicts['square_price'] for dicts in products]),2) # not useful for adjusted average  
 
 
 summaries = [
-    read_data(f"history/01-10-20/data-{i}.json")
+    read_data(f"history/01-10-20/data-{i}.json") # pointing at a specific date
     for i in range(len([1 for x in list(os.scandir("./history/01-10-20")) if x.is_file()]))
 ]
 
+headers = ["price","square_price","size"]
 
-def get_average_square_price():
-    ga=[
-    entry["square_price"] for summary in summaries for entry in summary
-    if entry["square_price"] > 150 and entry["square_price"] < 6000
-    ]
-    ga.sort()
+def get_averages():
+    for header in headers:    
+        ga=[
+        float(str(entry[header]).replace(",",".")) for summary in summaries for entry in summary
+        #if float(entry["price"]) > 8000 and float(entry["price"]) < 10000000 # TODO find a place for it
+        ]
+        ga.sort()
 
-    average = sum(ga)/len(ga)
-    median = ga[math.floor(len(ga)/2)]
+        average = sum(ga)/len(ga)
+        median = ga[math.floor(len(ga)/2)]
 
-    print("average square total: " + str(average))
-    print("median total: " + str(median))
-    cut = ga[math.floor(0.1*len(ga)):math.floor(0.9*len(ga))]
-    adjusted_average_price= sum(cut)/len(cut)
-    adjusted_median_price = cut[math.floor(len(ga)/2)]
-    print("adjusted average: " + str(round(adjusted_average_price,2)))
-    print("adjusted median: " + str(round(adjusted_median_price,2)))
-    print(len(ga))
-    print(len(cut))
+        print(f"average {header}: " + str(round(average,2)))
+        print(f"median {header}: " + str(round(median,2)))
+        cut = ga[math.floor(0.1*len(ga)):math.floor(0.9*len(ga))]
+        adjusted_average_price= sum(cut)/len(cut)
+        adjusted_median_price = cut[math.floor(len(ga)/2)]
+        print(f"adjusted average {header}: " + str(round(adjusted_average_price,2)))
+        print(f"adjusted median {header}: " + str(round(adjusted_median_price,2)))
+        print()
+        #print(len(ga))
+        #print(len(cut))
 
-def get_average_price():
-    ga=[
-    float(entry["price"]) for summary in summaries for entry in summary
-    if float(entry["price"]) > 8000 and float(entry["price"]) < 10000000
-    ]
-    ga.sort()
-
-    average = sum(ga)/len(ga)
-    median = ga[math.floor(len(ga)/2)]
-
-    print("average square total: " + str(average))
-    print("median total: " + str(median))
-    cut = ga[math.floor(0.1*len(ga)):math.floor(0.9*len(ga))]
-    adjusted_average_price= sum(cut)/len(cut)
-    adjusted_median_price = cut[math.floor(len(ga)/2)]
-    print("adjusted average: " + str(round(adjusted_average_price,2)))
-    print("adjusted median: " + str(round(adjusted_median_price,2)))
-    print(len(ga))
-    print(len(cut))
-
-get_average_price()
-get_average_square_price()
+get_averages()
 
