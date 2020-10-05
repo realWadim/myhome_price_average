fd11c9840c8bef7a03be4cd8f73fac213eb58e8e
Wadim Smuda <w.j.smuda@gmail.com>
2020-10-05 15:50:25 +0200

    edited scraper to create a fresh dir for each date

--- myhome_scraper.py
+++ myhome_scraper.py
@@ -1,8 +1,15 @@
 import requests
 import time
 import json
+import os
+from datetime import date
 
-for i in range(1000000):
+today = date.today()
+datetoday = today.strftime("%b-%d-%Y")
+
+os.mkdir(f"./history/{datetoday}/")
+
+for i in range(2000):
     url = f"https://www.myhome.ge/en/s/?Keyword=Tbilisi&AdTypeID=1&PrTypeID=1&Page={i}&mapC=41.73188%2C44.83688&mapZ=12&mapOp=0&EnableMap=0&cities=1996871&GID=1996871&Ajax=1"
 
     payload = {}
@@ -13,7 +20,7 @@ for i in range(1000000):
     response = requests.request("GET", url, headers=headers, data = payload)
 
     page = response.json()
-    with open(f"data-{i}.json", "w") as f:
+    with open(f"./history/{datetoday}/data-{i}.json", "w") as f:
         json.dump(page, f, indent=4)
     print(i, page["StatusCode"])
     time.sleep(1)
